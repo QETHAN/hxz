bee3756a2619cf7e3de5b8fc27c3c93ea189a14d
qbylucky <18600211621@163.com>
2017-08-21 18:16:49 +0800

    checkIfInWeixin

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,6 +6,18 @@ use app\admin\model\User as UserModel;
 
 class Index extends Controller
 {
+    protected $beforeActionList = [
+      'checkIfInWeixin'
+    ];
+
+    protected function checkIfInWeixin() 
+    {
+      $useragent = $_SERVER['HTTP_USER_AGENT'];  
+      if(strpos($useragent, 'MicroMessenger') === false && strpos($useragent, 'Windows Phone') === false ){  
+          echo " 非微信浏览器禁止访问";  
+      }
+    }
+
     public function index()
     {
       $users = UserModel::all();
